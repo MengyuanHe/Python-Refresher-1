1b97f783434dcf477b5083a3a4f8f87ed66304ab
MengyuanHe <72575835+MengyuanHe@users.noreply.github.com>
2020-12-05 22:07:06 -0500

    Update 595 refresher.py

--- 595 refresher.py
+++ 595 refresher.py
@@ -8,7 +8,29 @@ def plotsc():
     c=np.cos(x)
     plt.plot(x,s)
     plt.plot(x,c)
+    plt.plot(np.linspace(0, 0.5 * np.pi),
+             np.tan(np.linspace(0, 0.5 * np.pi)),
+             label='Tangent',
+             color='red')  # Added by Mengyuan He
+
+    plt.plot(np.linspace(0.5 * np.pi + 1e-10, 1.5 * np.pi),
+             np.tan(np.linspace(0.5 * np.pi + 1e-10, 1.5 * np.pi)),
+             color='red')  # Added by Mengyuan He
+
+    plt.plot(np.linspace(1.5 * np.pi + 1e-10, 2 * np.pi),
+             np.tan(np.linspace(1.5 * np.pi + 1e-10, 2 * np.pi)),
+             color='red')  # Added by Mengyuan He
+
+    plt.axhline(y=0.0, color='black')  # Added by Mengyuan He
+
+    plt.ylim(-1.00, 1.00)  # The original scale is from -1.0 to 1.0, # Added by Mengyuan He
+    plt.xlabel('x - axis')
+    plt.ylabel('y-axis')
+    plt.title('My lines')
+    plt.legend()
+
     plt.show()
 
 if __name__ == "__main__":
     plotsc()
+
